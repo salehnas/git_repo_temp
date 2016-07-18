1bdf96aca7076f1aaac9f80b287f225b9a9a1d3e
rami.saleh-nasser <rami.saleh-nasser@hpe.com>
2016-07-18 09:57:00 +0300

    adding testing

---
+++ src/test/java/failedTests.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by salehnas on 7/18/2016.
+ */
+public class failedTests {
+
+    @Test
+    public void test1() {
+        Assert.assertTrue("testing_issues", 1 == 2);
+    }
+}
